45312274eb5cb98f24a590f54a9a56078408b68a
Jose Alexis Correa valencia <jalexiscv@gmail.com>
2015-05-17 19:07:30 -0500

    Enlace de reconexion en caso de no hallarse una sesion activa.

--- formularios/solicitudes/equipo/equipo.xhr.php
+++ formularios/solicitudes/equipo/equipo.xhr.php
@@ -1,7 +1,6 @@
 <?php
 $root = (!isset($root)) ? "../../../../../" : $root;
 require_once($root . "modulos/solicitudes/librerias/Configuracion.cnf.php");
-$sesion=new Sesion();
 $validaciones=new Validaciones();
 /*
  * Copyright (c) 2013, Alexis
@@ -28,45 +27,10 @@ $validaciones=new Validaciones();
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-$usuario=Sesion::usuario();
-$v['uid']=$usuario['usuario'];
-$v['criterio']=$validaciones->recibir("criterio");
-$v['valor']=$validaciones->recibir("valor");
-$v['fechainicial']=$validaciones->recibir("fechainicial");
-$v['fechafinal']=$validaciones->recibir("fechafinal");
-$v['transaccion']=$validaciones->recibir("transaccion");
-$v['url']="modulos/solicitudes/formularios/solicitudes/equipo/equipo.json.php?"
-        . "uid=".$v['uid']
-        . "&criterio=".$v['criterio']
-        . "&valor=".$v['valor']
-        . "&fechainicial=".$v['fechainicial']
-        . "&fechafinal=".$v['fechafinal']
-        . "&transaccion=".$v['transaccion'];
 
-/** Creación de la tabla **/
-$tabla = new iTable(array("id" => time(), "url" => $v['url']));
-//$tabla->boton('btnCrear', 'Crear', '', "MUI.Usuarios_Usuario_Crear", "pNuevo");
-//$tabla->boton('btnVer', 'Historial', 'usuario', "MUI.Usuarios_Usuario_Consultar", "pAbrir");
-//$tabla->boton('btnModificar', 'Modificar', 'usuario', "MUI.Usuarios_Usuario_Modificar", "pEditar");
-//$tabla->boton('btnEliminar', 'Eliminar', 'usuario', "MUI.Usuarios_Roles_Rol_Eliminar", "pEliminar");
-$tabla->boton('btnFiltrar', 'Filtrar', '', "MUI.Solicitudes_Equipo_Complemento_Filtrar", "pBuscar");
-$tabla->boton('btnResponsables', 'Información', 'solicitud', "MUI.Solicitudes_Responsables", "pInformacion");
-$tabla->columna('cSolicitud', 'Solicitud', 'solicitud', 'string', '90', 'center', 'true');
-$tabla->columna('cCreador', 'Creador', 'creador', 'string', '100', 'left', 'true');
-$tabla->columna('cResponsable', 'Responsable', 'responsable', 'string', '90', 'left', 'true');
-$tabla->columna('cEquipo', 'Equipo', 'equipo', 'string', '90', 'center', 'true');
-$tabla->columna('cCodigo', 'Solicitud', 'codigo', 'string', '90', 'center', 'false');
-$tabla->columna('cDetalles', 'Detalles (Servicio+Categoria+Causal+Suscriptor)', 'detalles', 'string', '570', 'left', 'false');
-$tabla->columna('cRespuesta-Dias', '<a href=\"#\" onClick=\"MUI.Notificacion(\'Dias Habiles Trascurridos\');\">DHT</a>', 'respuesta-dht', 'string', '40', 'center', 'false');
-$tabla->columna('cEstados', 'Estados', 'estados', 'string', '90', 'center', 'false');
-$tabla->columna('cFecha', 'Radicación', 'radicacion', 'date', '90', 'center', 'false');
-$tabla->columna('cEquipo', 'Equipo', 'equipo', 'string', '90', 'center', 'false');
-//$tabla->columna('cCreador', 'Creador', 'creador', 'string', '90', 'center', 'false');
-$tabla->columna('cRespuesta', 'Respuesta', 'respuesta', 'string', '110', 'center', 'false');
-$tabla->columna('cRespuesta-Tipo','<a href=\"#\" onClick=\"MUI.Notificacion(\'Tipo de Respuesta\');\">TR</a>', 'respuesta-tipo', 'string', '45', 'center', 'false');
-$tabla->columna('cRespuesta-Fecha', '<a href=\"#\" onClick=\"MUI.Notificacion(\'Fecha de Respuesta\');\">FR</a>', 'respuesta-fecha', 'string', '90', 'center', 'false');
-//$tabla->columna('cNotificacion', 'Notificación', 'notificacion', 'string', '90', 'center', 'false');
-$tabla->columna('cNotificacionFecha','<a href=\"#\" onClick=\"MUI.Notificacion(\'Fecha de Notificación de Ejecución\');\">FNE</a>', 'notificacion-fecha', 'date', '90', 'center', 'false');
-$tabla->columna('cNotificacionTipo','<a href=\"#\" onClick=\"MUI.Notificacion(\'Tipo de Notificación\');\">TN</a>', 'notificacion-tipo', 'date', '45', 'center', 'false');
-$tabla->generar();
+if(Sesion::Iniciada()){
+  require_once($root."modulos/solicitudes/formularios/solicitudes/equipo/tabla.inc.php");
+}else{
+  require_once($root."modulos/aplicacion/formularios/sesion/reconexion/central.inc.php");
+}
 ?>
